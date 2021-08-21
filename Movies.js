d79cbcccbe05e6cd382fe087346791212edae1d3
Maruf Hasan <83651205+RRMovies@users.noreply.github.com>
2021-08-21 21:50:05 +0600

    Add files via upload

--- Movies.js
+++ Movies.js
@@ -4,6 +4,9 @@ const numA = list.getElementsByTagName("a");
 const resultsWrap = document.getElementsByClassName("resultsWrap")[0];
 const srcbtn = document.getElementById("srcbtn");
 const searchBox = document.getElementsByClassName("searchBox")[0];
+const imgcont = document.querySelectorAll(".imgContainer");
+
+//Auto-list
 
 for (i = 0; i < numA.length; i++) {
   numA[i].addEventListener("click", toggleListM);
@@ -20,10 +23,27 @@ function toggleListM() {
     list.classList.toggle("active", true);
   }
 }
-var imgcont = document.getElementsByClassName("imgContainer");
+
+//Putting elements to their pos
+
+fetch("Movies.json").then(Response => Response.json()).then(data => {
+  for (let i = 0; i < imgcont.length; i++) {
+    // console.log(imgcont.length);
+    let ev = eval("data." + imgcont[i].getAttribute("data-name"));
+    imgcont[i].setAttribute("id", ev.id);
+    imgcont[i].innerHTML = '<img src="' + ev.img + '" alt="' + ev.name + '"/><p>' + ev.name + '</p>';
+  }
+}).then(function(){
+  ImgResize();
+  searching();
+  });
+
+// fixing img size 
+function ImgResize(){
 for (var n = 0; n < imgcont.length; n++) {
   var img = imgcont[n].getElementsByTagName("img")[0];
   var w = (img.offsetWidth / 27) * 40;
   img.style.maxHeight = w + "px";
   img.style.minHeight = w + "px";
+}
 }
--- search.js
+++ search.js
@@ -1,54 +1,56 @@
 var arr = new Array;
 var arr2 = new Array;
-
-for (var n = 0; n < imgcont.length; n++) {
-  var p = imgcont[n].querySelector("p");
-  
-  var ntb = p.textContent.split(" ").join("_");
-  
-  imgcont[n].setAttribute("id", ntb);
-  arr.push(imgcont[n].getAttribute("id"));
-  arr2.push(imgcont[n].getAttribute("id").split("_").join(" "));
-}
-
 let search = document.getElementById("search");
 
-search.addEventListener("keyup", function() {
-  let result = [];
-  let input = search.value;
-  if (input.length) {
-    result = arr2.filter(function(item) {
-      return item.toLowerCase().includes(input.toLowerCase());
-    });
-  }
-  renderResult(result);
-});
+function searching() {
+  for (var n = 0; n < imgcont.length; n++) {
+    var p = imgcont[n].querySelector("p");
 
-function renderResult(result) {
-  if(result.length){
-    searchBox.classList.add("show");
-  }
-  else{
-    searchBox.classList.remove("show");
-  }
-  let contents = result.map(function(item) {
-    return `<li><a href="#${item.split(" ").join("_")}">${item}</a></li>`;
-  }).join("");
-  resultsWrap.innerHTML = `<ul>${contents}</ul>`
-}
+    var ntb = p.textContent.split(" ").join("_");
 
-srcbtn.addEventListener("click",function(){
-  try{
-  if(!resultsWrap.getElementsByTagName("ul")[0].children.length){
-    google();
+    imgcont[n].setAttribute("id", ntb);
+    arr.push(imgcont[n].getAttribute("id"));
+    arr2.push(imgcont[n].getAttribute("id").split("_").join(" "));
   }
-  }
-  catch(error){
-    alert("please search something");
-  }
-});
 
-function google(){
-  let url = "https://www.google.com/search?q="+encodeURIComponent(search.value);
-  window.open(url,"_self");
+
+  search.addEventListener("keyup", function() {
+    let result = [];
+    let input = search.value;
+    if (input.length) {
+      result = arr2.filter(function(item) {
+        return item.toLowerCase().includes(input.toLowerCase());
+      });
+    }
+    renderResult(result);
+  });
+
+  function renderResult(result) {
+    if (result.length) {
+      searchBox.classList.add("show");
+    }
+    else {
+      searchBox.classList.remove("show");
+    }
+    let contents = result.map(function(item) {
+      return `<li><a href="#${item.split(" ").join("_")}">${item}</a></li>`;
+    }).join("");
+    resultsWrap.innerHTML = `<ul>${contents}</ul>`
+  }
+
+  srcbtn.addEventListener("click", function() {
+    try {
+      if (!resultsWrap.getElementsByTagName("ul")[0].children.length) {
+        YouTube();
+      }
+    }
+    catch (error) {
+      alert("please search something");
+    }
+  });
+
+  function YouTube() {
+    let url = "https://www.youtube.com/channel/UCA3nZPJc-cokkEedCGjum1g/search?query=" + encodeURIComponent(search.value);
+    window.open(url, "_self");
+  }
 }
